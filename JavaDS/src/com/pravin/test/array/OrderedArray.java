82152fc2a37cc73ef7b3679eced5fc6359a3c247
Pravin <macaccpravin@gmail.com>
2017-02-18 20:38:15 +0530

    Cleaned up add

--- JavaDS/src/com/pravin/test/array/OrderedArray.java
+++ JavaDS/src/com/pravin/test/array/OrderedArray.java
@@ -3,36 +3,42 @@ package com.pravin.test.array;
 public class OrderedArray {
 	private long[] arrayItems;
 	private int mArraySize;
-	
-	public int size(){
+
+	public int size() {
 		return mArraySize;
 	}
 
-public OrderedArray(int size){
-		mArraySize =size;
+	public OrderedArray(int size) {
+		mArraySize = 0;
 		arrayItems = new long[size];
-		
-	}
-	public void insert(long value){
-		int j;
-		for(j=0;j<mArraySize;j++){
-			if(arrayItems[j]>value){
-				break;
-			}
-			System.out.println("");
-			for(int k=mArraySize;k>j;k--){
-				System.out.println("K Value "+k +"J value"+j);
-				arrayItems[k]=arrayItems[k-1];
-			}
-			arrayItems[j]=value;
-		}
-		mArraySize++;
+
 	}
 	
-	public void display(){
+	public void insertWithValue(int value){
+		int j;
+		for (j = 0; j < mArraySize; j++) {
+			if (arrayItems[j] > value) {
+				break;
+			}	
+				
+			
+			for (int k = mArraySize; k < j; k--) {
+				arrayItems[k] = arrayItems[k - 1];
+			}
+			
+			//arrayItems[j] = value;
+		}
+		arrayItems[j] = value;
+		mArraySize++;
+		
+	}
+
+	
+
+	public void display() {
 		System.out.println("Values in Array");
-		for(int index=0;index<mArraySize;index++){
-		System.out.print("Index :"+index + "- >" + arrayItems[index]);
+		for (int index = 0; index < mArraySize; index++) {
+			System.out.print(" "+arrayItems[index]);
 		}
 	}
 }
--- JavaDS/src/com/pravin/test/array/OrderedArrayApp.java
+++ JavaDS/src/com/pravin/test/array/OrderedArrayApp.java
@@ -3,9 +3,16 @@ package com.pravin.test.array;
 public class OrderedArrayApp {
 	public static void main(String[] args){
 		OrderedArray orderedArray= new OrderedArray(10);
+		orderedArray.insertWithValue(5);
 		orderedArray.display();
-		orderedArray.insert(10);
-		orderedArray.insert(5);
+		orderedArray.insertWithValue(7);
 		orderedArray.display();
+		orderedArray.insertWithValue(14);
+		orderedArray.display();
+		orderedArray.insertWithValue(24);
+		orderedArray.display();
+		orderedArray.insertWithValue(4);
+		orderedArray.display();
+	
 	}
 }
